f6b6fbd9b2b6ff9cbef4a9e4ac55022a686f284a
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2026-07-05 17:48:55 +1200

    ADD: pause menu open and close

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Pause : MonoBehaviour
+{
+    public bool gamePaused;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gamePaused = false;
+        gameObject.SetActive(gamePaused);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPause(InputAction.CallbackContext value)
+    {
+        if(value.started)
+        {
+            gamePaused = !gamePaused;
+            print("Is Game Paused? " + gamePaused);
+            gameObject.SetActive(gamePaused);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        gamePaused = false;
+        gameObject.SetActive(gamePaused);
+
+    }
+}
