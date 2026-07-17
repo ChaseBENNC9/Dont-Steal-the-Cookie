00a68c00026e0387e7472878e277a42bdd24fbee
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2026-07-18 11:58:47 +1200

    ADD: Doors can be opened and closed in the house

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,12 +6,17 @@ public class Door : InteractableObject
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Key myKey;
     public bool isLocked;
+    public Transform doorTransform;
     [SerializeField] private PlayerInventory playerInventory;
     protected void Start()
     {
         playerInventory = GameObject.Find("Player").GetComponent<PlayerInventory>();
         Debug.Assert(playerInventory != null, "Player does not have an inventory component attached");
         Debug.Assert(!isLocked || myKey != null, "Door requires a key to be locked!! " + this.gameObject.name);
+        if (doorTransform == null)
+        {
+            doorTransform = this.transform;
+        }
 
     }
 
@@ -24,33 +29,34 @@ public class Door : InteractableObject
 
     public override void Interact()
     {
-        base.Interact();
 
         if (isLocked)
         {
-
-
             if (playerInventory.currentItem != null)
             {
                 if (playerInventory.currentItem.GetComponent<InteractableObject>() == myKey)
                 {
-
-
                     isLocked = false;
-
                 }
-                else
-                {
-
-                }
-            }
-
-            else
-            {
             }
         }
         else
         {
+            base.Interact();
+            ToggleDoor();
+        }
+    }
+
+
+    public void ToggleDoor()
+    {
+        if(interacted)
+        {
+            doorTransform.Rotate(new Vector3(0,1,0),90);
+        }
+        else
+        {
+            doorTransform.Rotate(new Vector3(0,1,0),-90);
         }
     }
 }
