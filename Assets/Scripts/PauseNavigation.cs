359f2dc10edd6231d6b9caeb960987a14ebe53f0
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2026-07-18 23:15:42 +1200

    FIX: Pause can load options properly and unload it

---
+++ Assets/Scripts/PauseNavigation.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+public class PauseNavigation : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UnityEvent eventsLoadingOptions;
+    public UnityEvent eventsUnloadingOptions;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnSceneUnloaded(Scene scene)
+    {
+        if (scene.name == "Options")
+        {
+            if (SceneManager.GetActiveScene().name == "House")
+            {
+                ExitOptions();        // Your original method
+            }
+        }
+    }
+
+
+    public void LoadOptions()
+    {   
+        eventsLoadingOptions.Invoke();
+    }
+    public void ExitOptions()
+    {
+        eventsUnloadingOptions.Invoke();   
+    }
+}
